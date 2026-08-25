cca1eb53bfc36468b6fa9bcbcc2528f2a8cd6cd3
Bora Deren <129903320+boraderen@users.noreply.github.com>
2026-08-25 23:34:21 +0300

    Played with kairo and LLMs

--- data/generate_log.py
+++ data/generate_log.py
@@ -2,12 +2,12 @@ import rheon
 from datetime import datetime
 
 drifts = [
-        #{"type": "reassignment", "mode": "sudden", "drift_point": 0.5},
+        {"type": "reassignment", "mode": "sudden", "drift_point": 0.5},
         #{"type": "reassignment", "mode": "sudden", "drift_point": 0.33}
-        #{"type": "duration", "mode": "sudden", "drift_point": 0.4, "resources": ["res_01", "res_02"], "factor": 2.5},
-        #{"type": "pool_size", "mode": "sudden", "drift_point": 0.6, "delta": -2},
+        #{"type": "duration", "mode": "sudden", "drift_point": 0.2, "resources": ["res_01", "res_02"], "factor": 2.5},
+        #{"type": "pool_size", "mode": "sudden", "drift_point": 0.8, "delta": -2},
         #{"type": "workload", "mode": "sudden", "drift_point": 0.8, "workload_factor": 1.6},
-        {"type": "control_flow", "mode": "sudden", "drift_point": 0.75, "num_activities": 8, "tree_weights": {"sequence": 0.50, "choice": 0.48, "parallel": 0.1, "loop": 0.01}},
+        #{"type": "control_flow", "mode": "sudden", "drift_point": 0.50, "num_activities": 8, "tree_weights": {"sequence": 0.50, "choice": 0.48, "parallel": 0.1, "loop": 0.01}},
         #{"type": "amount", "mode": "sudden", "drift_point": 0.50, "mean": 3000.0, "variance": 40000.0},
         #{"type": "waiting_time", "mode": "sudden", "drift_point": 0.3, "mean": 45.0, "variance": 80.0}
     ]
@@ -15,8 +15,8 @@ drifts = [
 
 rheon.generate_log(
     drifts,
-    "./data/control-flow.xes",
-    num_traces=5000,
+    "./data/reassignment.xes",
+    num_traces=10000,
     num_activities=7,
     num_resources=7,
     seed=7,
